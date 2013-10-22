fae16d609f5bbb8c021dd0c42bb0235697b1eeb9
Goran Ehrsson <goran@technipelago.se>
2013-10-22 12:40:48 +0200

    Cleanup

--- CrmOrderGrailsPlugin.groovy
+++ CrmOrderGrailsPlugin.groovy
@@ -1,19 +1,29 @@
+/*
+ * Copyright (c) 2012 Goran Ehrsson.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 class CrmOrderGrailsPlugin {
-    // Dependency group
     def groupId = "grails.crm"
-    // the plugin version
-    def version = "1.0.8"
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.0 > *"
-    // the other plugins this plugin depends on
+    def version = "1.2.0-SNAPSHOT"
+    def grailsVersion = "2.2 > *"
     def dependsOn = [:]
-    // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/conf/ApplicationResources.groovy",
             "grails-app/services/grails/plugins/crm/order/TestSecurityService.groovy",
             "grails-app/views/error.gsp"
     ]
-
     def title = "Grails CRM Order Management Plugin"
     def author = "Goran Ehrsson"
     def authorEmail = "goran@technipelago.se"
@@ -22,8 +32,6 @@ Simple order management for Grails CRM.
 This plugin provides the "headless" part of Grails CRM order management (i.e domains and services).
 The companion plugin crm-order-ui provides user the interface for order management.
 '''
-
-    // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/crm-order"
     def license = "APACHE"
     def organization = [name: "Technipelago AB", url: "http://www.technipelago.se/"]
