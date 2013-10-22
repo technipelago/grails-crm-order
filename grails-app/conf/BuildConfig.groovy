2928991df7c658f07146ad0891ed0d089dbb208d
Goran Ehrsson <goran@technipelago.se>
2013-10-22 12:40:05 +0200

    Upgraded project to Grails 2.2.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,18 +1,12 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = "target"
 grails.project.target.level = 1.6
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.repos.default = "crm"
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
+    inherits("global") {}
     log "warn"
+    legacyResolve false
     repositories {
         grailsHome()
         mavenRepo "http://labs.technipelago.se/repo/crm-releases-local/"
@@ -21,17 +15,22 @@ grails.project.dependency.resolution = {
         mavenCentral()
     }
     dependencies {
+        test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
     }
 
     plugins {
         build(":tomcat:$grailsVersion",
-                ":release:2.2.1") {
+                ":release:2.2.1",
+                ":rest-client-builder:1.0.3") {
             export = false
         }
         runtime ":hibernate:$grailsVersion"
 
+        test(":spock:0.7") {
+            export = false
+            exclude "spock-grails-support"
+        }
         test(":codenarc:0.18.1") { export = false }
-        test(":spock:0.7") { export = false }
 
         compile "grails.crm:crm-core:latest.integration"
         runtime "grails.crm:crm-tags:latest.integration"
