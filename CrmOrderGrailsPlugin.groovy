b8b2252bfc3a80e2623fb5c621b119dcb1918c06
Goran Ehrsson <goran@technipelago.se>
2015-05-29 22:57:13 +0200

    Version 2.4.1 published to grails.org

--- CrmOrderGrailsPlugin.groovy
+++ CrmOrderGrailsPlugin.groovy
@@ -16,7 +16,7 @@
 
 class CrmOrderGrailsPlugin {
     def groupId = ""
-    def version = "2.4.1-SNAPSHOT"
+    def version = "2.4.1"
     def grailsVersion = "2.4 > *"
     def dependsOn = [:]
     def pluginExcludes = [
